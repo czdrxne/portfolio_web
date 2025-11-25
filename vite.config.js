66d4c3ee976f0449fb8bd21b226bc79887ab782a
czdrxne <czdrxne@gmail.com>
2025-11-25 23:22:55 +0800

    Development update. Added proper vite configuration.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-})
+  plugins: [svelte(), tailwindcss()],
+  server: { port: 5000, strictPort: true },
+  define: { __APP_ENV__: JSON.stringify(process.env.NODE_ENV || "development"), },
+  
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@lib": path.resolve(__dirname, "./src/lib"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@components": path.resolve(__dirname, "./src/lib/components"),
+      "@utilities": path.resolve(__dirname, "./src/lib/utilities"),
+    },
+  },
+
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: process.env.NODE_ENV !== "production",
+    minify: "esbuild",
+    target: "esnext",
+    chunkSizeWarningLimit: 1000,
+    esbuild: {
+      drop: process.env.NODE_ENV === "production" ? ["console", "debugger"] : [],
+    },
+  },
+});
